a1f8faf660a1af1fba7de20fcdc623d4866b8689
Brent <brentcarey1@outlook.com>
2020-03-25 22:06:53 +0000

    Implemented AJAX get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-    const data = [
-        {
-            "user": {
-                "name": "Newton",
-                "avatars": "https://i.imgur.com/73hZDYK.png"
-                ,
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": "https://i.imgur.com/nlhLi3I.png",
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        }
-    ]
+   
     const createTweetElement = (tweet) => {
         const { name, avatars, handle } = tweet.user;
         let html = `
@@ -56,7 +32,7 @@ $(document).ready(function () {
         let posted = $('.container').append(tweets);
         return posted;
     }
-    renderTweets(data);
+    
     $(".form").submit(function(event) {
         event.preventDefault()
         console.log('submit');
@@ -69,4 +45,20 @@ $(document).ready(function () {
             console.log('succes');
         });
     });
+
+        const loadTweets = ()=>{
+
+         $.ajax({
+            url: '/tweets',
+            type: "GET",
+            dataType:'JSON'
+        }).then(response => {
+            renderTweets(response);
+        });
+          
+        
+        
+    }
+    loadTweets();
+    
 });
